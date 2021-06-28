5abba272f1582c0f57d3c78bd0e2915558ef3caf
jinlliu <jinlliu@cisco.com>
2021-06-28 14:20:22 +0800

    add new software mysql

--- container/__init__.py
+++ container/__init__.py
@@ -1,4 +1,5 @@
 from .jenkins import Jenkins
+from .mysql import Mysql
 
 
 class ContainerFactory(object):
@@ -6,5 +7,7 @@ class ContainerFactory(object):
     def new_container(host_ip, host_user, host_pass, name, cpus, memory, container_type, version):
         if container_type == 'jenkins':
             return Jenkins(host_ip, host_user, host_pass, name, cpus, memory, container_type, version)
+        elif container_type == 'mysql':
+            return Mysql(host_ip, host_user, host_pass, name, cpus, memory, container_type, version)
         else:
             raise TypeError("Unknown container type")
---
+++ container/mysql.py
@@ -0,0 +1,45 @@
+from .container import Container
+
+
+class Mysql(Container):
+
+    def create(self):
+        """
+        Mysql specified create method, will get root password
+        :return: container status(address, port_mapping, root password), data format -> dict
+        """
+        self.callback = self._create()
+        self.read_root_password()
+
+        return {
+            "status": self.status,
+            "address": self.address,
+            "port_mapping": self.port_mapping,
+            "additional_infor": dict({"root_password": self.root_passwd})
+        }
+
+    def get(self):
+        """Get jenkins status information
+
+         return: container status, address, port_mapping, format dict
+         """
+        self.callback = self._get()
+        self.read_root_password()
+
+        return {
+            "status": self.status,
+            "address": self.address,
+            "port_mapping": self.port_mapping,
+            "additional_infor": dict({"root_password": self.root_passwd})
+        }
+
+    def read_root_password(self):
+        """
+        Read mysql root password
+        """
+        self.root_passwd = ""
+        for event in self.callback.host_ok:
+            if event['task'] == "Get mysql root password" and event['host'] == self.host_ip:
+                self.root_passwd = event['result']['stdout']
+            else:
+                pass
